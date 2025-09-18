5272b273a006608a68885e389e86302ac1fb6cea
blake terry <163461852+Blake-Terry@users.noreply.github.com>
2025-09-18 19:54:15 +0400

    Update app.js

--- app.js
+++ app.js
@@ -199,7 +199,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     refreshUI();
   });
   resetBtn.addEventListener('click', async ()=>{
-    if (!confirm("Reset this week's bookings?")) return;
+    if (!confirm('Reset this week\'s bookings?')) return;
     const weekKey = weekKeyForDate(currentWeekStart);
     await saveBookings(weekKey,{});
     refreshUI();
@@ -214,4 +214,5 @@ document.addEventListener("DOMContentLoaded", async () => {
   refreshUI();
 
 });
+
 
